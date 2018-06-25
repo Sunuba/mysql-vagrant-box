4eb7f9d775b6e385a519b3ceb32b171ea37527c0
Sunuba <madeinbaku@outlook.com>
2018-06-25 10:44:44 +0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y vim curl python-software-properties
+sudo apt-get update
+sudo apt-get -y install mysql-server
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password root"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+cp /vagrant/mysqld.cnf /etc/mysql/mysql.conf.d/
+sudo /etc/init.d/mysql restart
